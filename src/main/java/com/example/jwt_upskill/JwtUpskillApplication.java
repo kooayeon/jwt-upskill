6acf3fc4541286291fda7026ffd0522cebd5a605
kooayeon <@!cuzn2930>
2025-08-05 11:11:52 +0900

    first commit

---
+++ src/main/java/com/example/jwt_upskill/JwtUpskillApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_upskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUpskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtUpskillApplication.class, args);
+	}
+
+}
